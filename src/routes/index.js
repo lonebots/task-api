95d75dd15dba664197b60a8b834765deb1ef953e
lonebots <jishnusurajila@gmail.com>
2023-01-11 02:25:46 +0530

    route: global route updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import userRoute from './user.routes.js'
+import adminRoute from './auth.routes.js'
 
 const Router = express.Router();
 
-//route prefix
-//add user routes 
-Router.use('/api', userRoute)
-// index route
-Router.get('/', (req, res) => { res.status(200).json({ "success": true, "message": "Server succesfully started and running" }) })
 
 // health check
 Router.route('/healthcheck').get((req, res) => res.status(200).send("server up and running"))
+Router.get('/', (req, res) => { res.status(200).json({ "success": true, "message": "Server succesfully started and running" }) })
+
+
+// user route
+Router.use('/api', userRoute)
+
+// admin route
+Router.use('/api/admin', adminRoute)
 
 
 export default Router;
