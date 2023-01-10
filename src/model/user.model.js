8ccd5d7ab8e171a3b943d59dcee0d8f623018b6e
lonebots <jishnusurajila@gmail.com>
2023-01-11 02:24:21 +0530

    feat: user model updated

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import argon2 from 'argon2'
 
 //user schema
 const userSchema = new mongoose.Schema({
@@ -26,5 +27,24 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 })
 
+// user schema pre save hook to hash password 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const hash = await argon2.hash(this.password); //hashing the password
+    this.password = hash;
+    return next();
+})
+
+//utility to compare provided password and hash
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        return await argon2.verify(this.password, candidatePassword)
+    } catch (error) {
+        console.log('Could not verify user', error)
+        return false;
+    }
+}
 
 export default mongoose.model("User", userSchema);
