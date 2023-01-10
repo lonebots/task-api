a4153379182d9ba3daa93a23fdd2977e8fda4d3d
lonebots <jishnusurajila@gmail.com>
2023-01-10 20:13:57 +0530

    chore: folder restructuring, update start command

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ const Router = express.Router();
 //route prefix
 //add user routes 
 Router.use('/api', userRoute)
+// index route
+Router.get('/', (req, res) => { res.status(200).json({ "success": true, "message": "Server succesfully started and running" }) })
 
 // health check
 Router.route('/healthcheck').get((req, res) => res.status(200).send("server up and running"))
